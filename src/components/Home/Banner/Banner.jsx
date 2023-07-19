4af694266339c40844bfec559c1207c6b7d65b98
Shivanshu <pshivanshu99@gmail.com>
2023-07-20 01:40:58 +0530

    first commit

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>BIG BILLION SALE</h1>
+                    <p>
+                        An online store where you can get your Dream Gadgets on high Discount.
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Home/Category/Category.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import "./Category.scss";
+
+const Category = ({ categories }) => {
+    const navigate = useNavigate();
+    return (
+        <div className="shop-by-category">
+            <div className="categories">
+                {categories?.data?.map((item) => (
+                    <div
+                        key={item.id}
+                        className="category"
+                        onClick={() => navigate(`/category/${item.id}`)}
+                    >
+                        <img
+                            src={
+                                
+                                item?.attributes?.image?.data[0]?.attributes?.url
+                            }
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Category;
